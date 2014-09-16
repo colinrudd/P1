bb0c5a7d068c596a0d49a5e52541c83d237ed660
Colin Rudd <colindrudd@gmail.com>
2014-09-16 18:09:07 -0400

    Added index.php, style.css and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project 1
+
+Portfolio for Dynamic Web Applications Projects
---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<!--
+	Author: Colin Rudd
+-->
+<head>
+ 
+	<title>Colin Rudd P1 Portfolio</title>
+	<meta charset='utf-8'>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+			
+</head>
+
+<body>
+
+	<h1>Colin Rudd</h1>
+	<h1>Dynamic Web Applications</h1>
+
+	<h2>Project 2</h2>
+	<h2>Project 3</h2>
+	<h2>Project 4</h2>
+
+	<h3>In 2+ sentences describe who you are and what you do.</h3>
+	<h3>Why are you taking this course?</h3>
+	<h3>What previous experience do you have with programming?</h3>
+	<h3>Are you working on a Mac, PC, or some other platform?</h3>
+	<h3>Anything else we should know?</h3>
+
+</body>
+
+</html>
