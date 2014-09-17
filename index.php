909fb00d0355683ed6f5d103a79119a90ba1763b
Colin Rudd <colindrudd@gmail.com>
2014-09-16 22:01:13 -0400

    modified index.php and style.css added images/colin.jpg

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 -->
 <head>
  
-	<title>Colin Rudd P1 Portfolio</title>
+	<title>Colin Rudd E-15 Portfolio</title>
 	<meta charset='utf-8'>
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 			
@@ -17,15 +17,30 @@
 	<h1>Colin Rudd</h1>
 	<h1>Dynamic Web Applications</h1>
 
+	<p><img src="images/colin.jpg" alt="Colin Rudd" title="Colin Rudd" /></p>
+
 	<h2>Project 2</h2>
+	<p><a href="http://p2.colinrudd.me">Project 2 website</a></p>
+	<p><a href="https://github.com/colinrudd/P2">GitHub</a></p>
 	<h2>Project 3</h2>
+	<p><a href="http://p3.colinrudd.me">Project 3 website</a></p>
+	<p><a href="https://github.com/colinrudd/P3">GitHub</a></p>
 	<h2>Project 4</h2>
+	<p><a href="http://p4.colinrudd.me">Project 4 website</a></p>
+	<p><a href="https://github.com/colinrudd/P4">GitHub</a></p>
 
 	<h3>In 2+ sentences describe who you are and what you do.</h3>
+	<p>My name is Colin and I live in Concord, Ma. I worked at Iris Associates in Westford, Ma for 5 years in the late 90's.
+	Since then I have been working as a commercial painter. I started taking classes at Harvard Extension School in 2010.</p>
 	<h3>Why are you taking this course?</h3>
+	<p>I enjoyed CSCI E-12 Fundamentals of Website Development and want to learn more about creating applications.</p>
 	<h3>What previous experience do you have with programming?</h3>
+	<p>CSCI E-12, CSCI E-10a</p>
 	<h3>Are you working on a Mac, PC, or some other platform?</h3>
+	<p>PC</p>
 	<h3>Anything else we should know?</h3>
+	<p>I sometimes stream on Twitch as <a href="http://www.twitch.tv/kosh1203">Kosh1203</a></p>
+	<p>I am currently playing Marvel Heroes 2015 and Hearthstone while waiting for the next World of Warcraft expansion.</p>
 
 </body>
 
