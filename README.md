317361964c423679a8aea5e25406627c77776333
Colin Rudd <colindrudd@gmail.com>
2014-09-18 15:21:23 -0400

    modified index.php, style.css and README.md

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Project 1
-
-Portfolio for Dynamic Web Applications Projects
+# Project 1  
+  
+Title: Portfolio for Dynamic Web Applications Projects  
+  
+URL: [p1.colinrudd.me](http://p1.colinrudd.me)  
+  
+Description: Portfolio for future projects in CSCI E-15 Dynamic Web Applications.  
+  
+Demo Info: None  
+  
+Extra Details:  
+  
+* Google Web font Lato included.  
+* Pure CSS included.
--- index.php
+++ index.php
@@ -2,46 +2,50 @@
 <html lang="en">
 
 <!--
-	Author: Colin Rudd
+Author: Colin Rudd
 -->
+
 <head>
- 
-	<title>Colin Rudd E-15 Portfolio</title>
+	<title>Colin Rudd CSCI E-15 Portfolio</title>
 	<meta charset='utf-8'>
-	<link rel="stylesheet" type="text/css" href="style.css"/>
-			
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+
 </head>
 
 <body>
-
-	<h1>Colin Rudd</h1>
-	<h1>Dynamic Web Applications</h1>
+	<h3>Colin Rudd</h3>
+	<h3>Dynamic Web Applications Portfolio</h3>
 
 	<p><img src="images/colin.jpg" alt="Colin Rudd" title="Colin Rudd" /></p>
 
-	<h2>Project 2</h2>
-	<p><a href="http://p2.colinrudd.me">Project 2 website</a></p>
-	<p><a href="https://github.com/colinrudd/P2">GitHub</a></p>
-	<h2>Project 3</h2>
-	<p><a href="http://p3.colinrudd.me">Project 3 website</a></p>
-	<p><a href="https://github.com/colinrudd/P3">GitHub</a></p>
-	<h2>Project 4</h2>
-	<p><a href="http://p4.colinrudd.me">Project 4 website</a></p>
-	<p><a href="https://github.com/colinrudd/P4">GitHub</a></p>
+	<h3>Project 2</h3>
+	<p><a class="pure-button" href="http://p2.colinrudd.me">Project 2 website</a> 
+	<a class="pure-button" href="https://github.com/colinrudd/P2">GitHub</a></p>
+	<h3>Project 3</h3>
+	<p><a class="pure-button" href="http://p3.colinrudd.me">Project 3 website</a> 
+	<a class="pure-button" href="https://github.com/colinrudd/P3">GitHub</a></p>
+	<h3>Project 4</h3>
+	<p><a class="pure-button" href="http://p4.colinrudd.me">Project 4 website</a> 
+	<a class="pure-button" href="https://github.com/colinrudd/P4">GitHub</a></p>
 
 	<h3>In 2+ sentences describe who you are and what you do.</h3>
-	<p>My name is Colin and I live in Concord, Ma. I worked at Iris Associates in Westford, Ma for 5 years in the late 90's.
-	Since then I have been working as a commercial painter. I started taking classes at Harvard Extension School in 2010.</p>
+	<p>My name is Colin and I live in Concord, Ma. I worked at Iris Associates in Westford, Ma 
+	for 5 years in the late 90's. Since then I have been working as a commercial painter. I started 
+	taking classes at Harvard Extension School in 2010.</p>
 	<h3>Why are you taking this course?</h3>
-	<p>I enjoyed CSCI E-12 Fundamentals of Website Development and want to learn more about creating applications.</p>
+	<p>I enjoyed CSCI E-12 Fundamentals of Website Development and want to learn more about 
+	creating applications.</p>
 	<h3>What previous experience do you have with programming?</h3>
 	<p>CSCI E-12, CSCI E-10a</p>
 	<h3>Are you working on a Mac, PC, or some other platform?</h3>
 	<p>PC</p>
 	<h3>Anything else we should know?</h3>
-	<p>I sometimes stream on Twitch as <a href="http://www.twitch.tv/kosh1203">Kosh1203</a></p>
-	<p>I am currently playing Marvel Heroes 2015 and Hearthstone while waiting for the next World of Warcraft expansion.</p>
-
+	<p>I sometimes stream on Twitch as <a class="pure-button" href="http://www.twitch.tv/kosh1203">Kosh1203</a></p>
+	<p>I am currently playing Marvel Heroes 2015 and Hearthstone while waiting for the next 
+	World of Warcraft expansion.</p>
 </body>
 
 </html>
